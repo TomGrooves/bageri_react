cb3b6157f89d2fb4c49432e2f7f6ccebf8bf6f4a
TomGrooves <kasperfryd@gmail.com>
2020-05-27 18:14:14 +0200

    Updates

---
+++ precache-manifest.e12b2e13e00dcf94cdfb815d8ac7471f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf3062913426dc54bc00c066f877827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5653d37ce7159396cd20",
+    "url": "/static/css/main.86c8901b.chunk.css"
+  },
+  {
+    "revision": "e2b023601079d53e8968",
+    "url": "/static/js/2.50d61fc7.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.50d61fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5653d37ce7159396cd20",
+    "url": "/static/js/main.8739df9b.chunk.js"
+  },
+  {
+    "revision": "e2660d2954b1dba85a3a",
+    "url": "/static/js/runtime-main.4e469333.js"
+  },
+  {
+    "revision": "a48604531d44bd989a4eed6d41020fc3",
+    "url": "/static/media/1.a4860453.png"
+  },
+  {
+    "revision": "215cb199b39e102848f2617077971d5d",
+    "url": "/static/media/2.215cb199.png"
+  },
+  {
+    "revision": "bf1e10afec3dc0a3dc27aaa1a0c7e948",
+    "url": "/static/media/3.bf1e10af.png"
+  },
+  {
+    "revision": "2daa8a211c2ceb50d1d7026f3e803c2a",
+    "url": "/static/media/4.2daa8a21.png"
+  },
+  {
+    "revision": "a5b1180129f925bc5b71cb6c5e35bb7a",
+    "url": "/static/media/5.a5b11801.png"
+  },
+  {
+    "revision": "259c353557726351bb1dca65b51b3bd7",
+    "url": "/static/media/6.259c3535.png"
+  },
+  {
+    "revision": "1d39eb43dc5cb99b7b019783bb042834",
+    "url": "/static/media/7.1d39eb43.png"
+  },
+  {
+    "revision": "bb2066a6192461aaca272e9725d61956",
+    "url": "/static/media/8.bb2066a6.png"
+  },
+  {
+    "revision": "82ca3e782131c60cf1ea78372463275d",
+    "url": "/static/media/article.82ca3e78.jpg"
+  },
+  {
+    "revision": "797c9c26b73362a15f3b705b89bb8e15",
+    "url": "/static/media/article2.797c9c26.jpg"
+  },
+  {
+    "revision": "97589bdc71c4887651d5bddec9a7634d",
+    "url": "/static/media/article3.97589bdc.jpg"
+  },
+  {
+    "revision": "da61eeadaf8f6de68b05518ac5da3184",
+    "url": "/static/media/mapsample.da61eead.png"
+  },
+  {
+    "revision": "e1d444185be29ab1c18629547c745565",
+    "url": "/static/media/newsletterbg.e1d44418.jpg"
+  },
+  {
+    "revision": "68623e180f7cd44dffcf01847aa71fbf",
+    "url": "/static/media/slide1.68623e18.jpg"
+  },
+  {
+    "revision": "1250fba204cce67415a3fd24bdeca835",
+    "url": "/static/media/slide2.1250fba2.jpg"
+  },
+  {
+    "revision": "4b6c69dbb9b7e595b4f86c3fb1cb51da",
+    "url": "/static/media/slide3.4b6c69db.jpg"
+  }
+]);
